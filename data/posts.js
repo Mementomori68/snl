a27cbe650394b30396249028b84f55b7e090a519
sn <166755033+Mementomori68@users.noreply.github.com>
2026-09-21 17:43:38 +0300

    Update posts

--- data/posts.js
+++ data/posts.js
@@ -1,26 +1,5 @@
 const POSTS = [
     {
-        id: "2026-09-21-qkho",
-        title: "test",
-        text: "Всем привет! Тестирую приложение для вгрузки постов.",
-        date: "2026-09-21",
-        image: "images/post-2026-09-21-01.png"
-    },
-    {
-        id: "2026-09-21-ha48",
-        title: "test + post",
-        text: "kek",
-        date: "2026-09-21",
-        image: "images/post-2026-09-21-01.jpg"
-    },
-    {
-        id: "2026-09-21-7a7z",
-        title: "ff f f f",
-        text: "f f f f f f",
-        date: "2026-09-21",
-        image: null
-    },
-    {
         id: "первый-пост",
         title: "Первый пост",
         text: "Текст поста",
